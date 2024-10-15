a1e6e7d253dfe57dacc4942b3db7ad1b17fd8638
francis chapet <francis.chapet@doga.fr>
2024-10-15 15:52:33 +0200

    Réduction du projet aux seuls fichiers utiles à la solution actuelle

--- main.cpp
+++ main.cpp
@@ -25,14 +25,40 @@
  */
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
 
-extern int main1(int argc, char * argv[]);
-extern int main2();
-extern int main3(int argc, char *argv[]);
-
+#include "d_samba_client.h"
 
 int main(int argc, char * argv[])
 {
-    // return main2();
-    return main3(argc, argv);
+    // Initialiser l'application Qt
+    QGuiApplication app(argc, argv);
+
+    // Créer un client DSambaClient
+    DSambaClient sambaClient;
+
+    // Enregistrer la classe DSambaClient pour une utilisation directe dans QML
+    qmlRegisterType<DSambaClient>("SambaLib", 1, 0, "DSambaClient");
+
+    // Initialiser l'interface QML
+    QQmlApplicationEngine engine;
+
+    // Exposer l'instance de DSambaClient au contexte QML
+    engine.rootContext()->setContextProperty("sambaClient", & sambaClient);
+
+    // Charger le fichier QML principal
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(
+        & engine,
+        & QQmlApplicationEngine::objectCreated,
+        & app,
+        [url](QObject * obj, const QUrl & objUrl) {
+            if ((! obj) && url == objUrl)
+                QCoreApplication::exit(-1);
+        },
+        Qt::QueuedConnection);
+    engine.load(url);
+
+    // Exécuter l'application Qt
+    return app.exec();
 }
